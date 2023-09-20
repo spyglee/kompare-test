00f80f215b23352c31cad954f632d180db3b82d6
bahrov_m <37171644+bahrov@users.noreply.github.com>
2023-09-20 22:16:22 +0200

    added mongodb

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import { Paper } from '@mui/material'
+import { useEffect, useMemo } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const getTestData = async () => {
     try {
       const data = await fetch('http://localhost:8000/')
@@ -16,33 +16,22 @@ function App() {
     }
   }
 
+  const paperStyle = useMemo(() => ({
+    minWidth: '100vw',
+    minHeight: '100vh',
+    mt: -2,
+    mb: -2,
+    borderRadius: 0,
+  }), []);
+
   useEffect(() => {
     getTestData()
   }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Paper sx={paperStyle}>
+      <p>hello world</p>
+    </Paper>
   )
 }
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,33 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import express, {
+    Express,
+    Request,
+    Response
+  } from 'express'
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express()
+const port = process.env.PORT
+
+mongoose.connect(process.env.MONGO || '')
+const db = mongoose.connection
+
+db.on('error', (error) => {
+  console.log(error)
+})
+
+db.once('open', () => console.log('Connected to database'))
 
 app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   console.log(req)
-  res.send('Express + TypeScript Server');
-});
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
